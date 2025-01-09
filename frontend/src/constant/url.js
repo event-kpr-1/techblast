1601048cbd1d23e0e3e2f68dfc4110e0658aa5f9
Jainethin-M <jainethin59@gmail.com>
2025-01-09 21:37:44 +0530

    url

--- frontend/src/constant/url.js
+++ frontend/src/constant/url.js
@@ -1,6 +1,5 @@
-// export const baseURL = process.env.NODE_ENV !== 'production'?'http://localhost:9000':'https://event-kpr.onrender.com'
 
-// export const baseURL = 'https://event-kpr.onrender.com'
-export const baseURL = 'http://localhost:9000'
+export const baseURL = 'https://techblast.onrender.com'
+// export const baseURL = 'http://localhost:9000'
 
 export const eventURL = '6773f90db3f9eba56565cd25'
