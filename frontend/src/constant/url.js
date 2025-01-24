a87b7f109a000ba00ea81a71c7f8495721ba22c4
Thirumoorthy369 <mr.thirumoorthys@gmail.com>
2025-01-24 11:35:58 +0530

    url

--- frontend/src/constant/url.js
+++ frontend/src/constant/url.js
@@ -2,4 +2,4 @@
 export const baseURL = 'https://techblast.onrender.com'
 // export const baseURL = 'http://localhost:9000'
 
-export const eventURL = '6773f90db3f9eba56565cd25'
+export const eventURL = '679314ccfe9b048d35c196c1'
